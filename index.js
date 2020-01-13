c2db78b5065f05e9004e9b3027c7be687f536d6e
Recep Öztürk <turkishteacher23@gmail.com>
2020-01-13 20:55:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+const eventName = "surprise";
+
+function writeCards(names,eventName){
+  
+	let thnkList = [];
+	for (let i = 0; i< names.length;i++){
+		
+		 thnkList.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+
+
+	}return thnkList;
+	
+}
+
+writeCards(names,eventName);
+
+function countDown(i ){
+  // let nos = Number;
+  // let i = 10;
+  while (i >= 0){
+  console.log(i--);
+  }i--;
+}
+
+countDown(11);
+
+
