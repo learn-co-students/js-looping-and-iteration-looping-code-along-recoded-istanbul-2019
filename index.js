2c7ae2f485b23c09b24bb0b803cb6f9499261ccc
Oktay Ibis <oktayibis@gmail.com>
2020-01-18 21:59:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(names, reason){
+    
+    for (let index = 0; index < names.length; index++){
+
+
+          names[index]= `Thank you, ${names[index]}, for the wonderful ${reason} gift!`
+       
+       
+    }
+    return names;
+    
+}
+
+
+function countDown(number){
+    while(number > -1){
+        console.log(number);
+        number--;
+    }
+}
+
+countDown();
+const mess = writeCards().push;
