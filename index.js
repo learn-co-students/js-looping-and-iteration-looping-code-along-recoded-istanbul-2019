a943d63ca9dd7df4b3e1585e713ae85f9c31ca9c
learn <is4syria@gmail.com>
2020-01-21 05:34:57 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(cards, eventName) {
+    for (let i = 0; i < cards.length; i++) {
+        cards[i]= 'Thank you, '+cards[i]+ ', for the wonderful '+ eventName+ ' gift!';
+    }
+   
+    return cards;
+  }
+
+  writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+  function countDown(number) {
+   
+    while (number >= 0) {
+        console.log(number--);
+    }
+  }
+  countDown(10);
