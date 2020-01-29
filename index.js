d0c1c0abe188264676d53d3664791b8d1112bfbd
Hakan Yildiz <hakan@re-coded.com>
2020-01-29 09:41:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const arrayNames = [ 'Lisa', 'Kaitlin', 'Jan' ];
+const congrating = 'surprise';
+
+function writeCards(arrayNames, congrating){
+  let arr = [];
+  for (let i = 0; i < arrayNames.length; i++){
+    
+    arr.push(`Thank you, ${arrayNames[i]}, for the wonderful ${congrating} gift!`) ;
+  }
+return arr;
+}
+
+let intNumber = 10;
+function countDown(intNumber){
+  
+  while (intNumber >= 0){
+    console.log(intNumber--);
+  }
+}
