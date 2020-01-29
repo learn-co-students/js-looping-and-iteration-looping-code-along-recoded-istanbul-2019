955464cbd4880e4f4262b07aad33bf92837e912f
merve gumuskaya <mervegumuskaya@gmail.com>
2020-01-29 19:04:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const people = ["Lisa", "Kaitlin", "Jan"];
+const event = "surprise"
+let congMessage = people.slice();
+
+function writeCards(people, event) {
+    for (let i = 0; i < people.length; i++) {
+        congMessage[i] = (`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+    } 
+    return congMessage;
+}
+
+let num = 0;
+function countDown(num) {
+    while(num!=0) {
+        console.log(num);
+        num = num-1;
+    }
+    console.log(num);
+}
