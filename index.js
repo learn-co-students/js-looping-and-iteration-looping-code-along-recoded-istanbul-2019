82efa430392c6ece29a8d0052580db10d1644fb6
allan <allansaleh95@gmail.com>
2020-03-23 22:57:01 +0300

    Testing looping and iteration code

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+let people = ['Ada', 'Brandon', 'Ali'];
+let occasion = 'birthday';
+
+function writeCards(people,occasion)
+{
+    for(let i = 0; i<people.length; i++){
+        console.log(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`);
+    }
+}
+
+let number = 10;
+
+function countDown(number){
+    while(number>0){
+        console.log(`${number}`);
+        number--;
+    }
+}
