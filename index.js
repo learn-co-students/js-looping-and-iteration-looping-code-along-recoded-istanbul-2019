b46b4ff255685edc7021d56b5cd46da65d31f501
allan <allansaleh95@gmail.com>
2020-03-23 23:14:39 +0300

    Rearranging the solution to meet the tests

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Code your solutions in this file
 
-// let people = ['Ada', 'Brandon', 'Ali'];
-// let occasion = 'birthday';
+let people = ['Ada', 'Brandon', 'Ali'];
+let occasion = 'birthday';
 
 function writeCards(people,occasion)
 {
     let thankYouMessages;
     for(let i = 0; i<people.length; i++){
-        thankYouMessages = thankYouMessages.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`);
+        thankYouMessages[i] = `Thank you, ${people[i]}, for the wonderful ${occasion} gift!`;
     }
     return people;
 }
