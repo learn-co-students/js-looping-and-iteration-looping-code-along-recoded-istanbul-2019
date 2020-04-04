2acfe990175280cf3ad4d4f202cc31d8c6ec70fb
Bilge <lagala999@gmail.com>
2020-04-04 10:59:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+nameList=["Ada", "Brendan", "Ali"];
+whatToWrite="birthday";
+messageList = writeCards(nameList, whatToWrite);
+console.log(messageList);
+countDown(10);
+
+function countDown(number) {
+  while (0<=number) {
+    console.log(number);
+    number--;
+  }
+
+}
+function writeCards(nameList, whatToWrite) {
+  let message=[""];
+  for (let i = 0; i < nameList.length; i++) {
+    message.push("Thank you, "+ nameList[i] +", for the wonderful "+ whatToWrite+ " gift!");
+  }
+  message.shift();
+  return message;
+
+
+}
