5717b1e4aa31ddd399607184cf2d55e2789a3013
aydin <aydindavut14@gmail.com>
2020-04-04 12:34:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(Names, eventName) {
+    let thanks = [];
+    for (let i = 0; i < Names.length; i++) {
+        thanks.push(`Thank you, ${Names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thanks;
+}
+
+
+
+
+
+
+
+
+
+function countDown(counter) {
+    while (counter >= 0) {
+        console.log(counter)
+        counter--;
+    }
+}
+
+countDown(10);
