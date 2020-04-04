89d7311d89c5d4a9621fc531aad451976737584d
mira_arar <mira.ar230@gmail.com>
2020-04-04 04:58:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+var messages =[];
+
+function writeCards(names,event) {
+    for (let i = 0; i < names.length; i++) {
+      messages[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+
+writeCards(names,'surprise');
+
+function countDown(num){
+    while(num>=0){
+        console.log(num);
+        num--;
+    }
+    
+}
+countDown(10);
