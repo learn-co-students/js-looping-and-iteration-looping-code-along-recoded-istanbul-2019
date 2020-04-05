6eae7fbbd708fe1e18bf0cf4830561247b70bd2f
basil-kawak <basilalisa0@gmail.com>
2020-04-06 01:58:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+
+
+let x = ["Ada", "Brendan", "Ali"];
+let y = "birthday";
+let z = [];
+function writeCards(x,y) {
+    for(let i = 0; i < x.length; i++ ) {
+         z[i] =`Thank you, ${x[i]}, for the wonderful ${y} gift!`;
+         }
+     return z;
+    }
+    console.log(writeCards(x,y));
+
+
+// let countup = 10;
+function countDown(num) {
+    while (num >= 0) {
+    console.log(num--);
+  }
+  }
+  countDown(10)
