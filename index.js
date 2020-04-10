ac6cbcd24a20cc72f40576d20da2926a6af94084
tugba <ttugbasen@gmail.com>
2020-04-10 18:16:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+ const cards = ["Lisa", "Kaitlin", "Jan"] [cards(3)]
+ 
+function writeCards(cards) {
+
+  for (let i = 0; i < cards.length; i++) {
+  
+    `Thank you ${cards[i]} for the wonderful birthday gift!`
+  
+  }
+  return cards 
+ 
+  
+
+
+
+
+    /* function countDown (["Ayse", "Zeynep", "Cansu"], "birthday");
+    for (let i = 0, i < countDown.length; i++) {
+        console.log(`Thank you {countDown[i]} for the wonderful birthday gift!`);
+    } */
+
+let countDown = 20;
+while (countDown >= 0) {
+    console.log(countDown--)
+}
+    
+}
