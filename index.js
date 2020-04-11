246eb43cc5b6274c53f668180af4e1a0da280a8b
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-04-11 17:11:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const namesArray = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards (namesArray, event) {
+
+    let thanksArray = [];
+    for (let i = 0 ; i < namesArray.length ; i++) {
+        thanksArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return thanksArray;
+}
+
+function countDown (number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
